3a40a09fe652b0c647e04d588a82fb5b61716d45
Munoz <152181111+Kvrmea@users.noreply.github.com>
2024-04-04 20:52:36 +0200

    Add files via upload

---
+++ Azerty-OpenClassRoom/scripts/config.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette", "Diplôme", "JavaScripts", "HTML", "CSS"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste.", "Merci pour le poisson.", "On ne se baigne jamais deux fois dans le même fleuve.", "L'homme est un animal politique", "Le courage n'est pas l'absence de peur, mais la capacité de vaincre ce qui fait peur."]
